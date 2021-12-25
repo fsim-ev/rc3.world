f6a71302d66a5f90a4111b47e8e829e8ff4ba45c
Eeeeeelena <81362615+Eeeeeelena@users.noreply.github.com>
2021-12-25 22:10:23 +0100

    winter: boden

---
+++ tilesets/Tilesets/terrain-map-v7.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="terrain-map-v7" tilewidth="32" tileheight="32" tilecount="15744" columns="16">
+ <image source="../trees/lpc-jungle-ruins/preview/terrain-map-v7.png" width="512" height="31488"/>
+</tileset>
